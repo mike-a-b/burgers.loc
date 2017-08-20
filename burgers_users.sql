21876cd591994b3cff95acd57bc2aa37d1e7d5b7
mike <mike@mike-desktop.Dlink>
2017-08-20 21:23:10 +0300

    dumps db add

---
+++ burgers_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers.users (email, name, phone) VALUES ('mike.b@mail.ru', 'mike', '9105005555');
+INSERT INTO burgers.users (email, name, phone) VALUES ('vova.a@mail.ru', 'vova', '9668883344');
